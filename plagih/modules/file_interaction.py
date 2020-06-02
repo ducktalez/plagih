- tree complexity + stderror variance - plots with numpy-data one step earlier - fix: removed a wrong operators_yaml save
- fix: remove whitespaces from loaded tree-csv
- op-array realigned, renamed 'latex'->'latex1' and added latex function
- added pop-tree-complexity plot with stderror marked

0.95 Beautifying IB runs- choosing functions with probability list
instead of multiple adds
- time-limit for historic observation values
- filter operator for interchanging observations from different times
- env_variables are enhanced with the time-component of observations
- plots are filled with no color and no inbetween bars
- plot bins get adjusted automatically for discrete action range

0.94.3 Preparing IB combined run (running, testing)
- added some files to the dict to start runs from anywhere
- action selection bug fixed
- starting a run to solve for a specific eval_action (if needed)
- adjusting some non-reference options
- Loading with pandas: Got rid of the old loading procedure

0.94.2
- outsourced the kernel result from tf evaluation in pairwise fitness
- found missing op-pycode entry at 'Notb'
- changing plots (plot -> fix, ax)
- histograms v2 DONE! saving histograms for all dimensions in agents

- some fixes
- next steps making numpy-trees -> tree class
- updated sympify extras
- some todo cleanup
- IB (Industrial Benchmark) improved- normalisation seems wrong for p...

0.94.1
- restructured run-files
- alpha version for weighted distance
- new plots
- sympify bugs fixed with "not"
- added 'Square' to standard operators
0.94
- test v2 done
- new auto agents with sarsa done (2)
- replaced 'not' with 'Notb'
- weird bug: 'Not(a)' -> ~(a) in sympy. using 'not' now. srsly.

0.93
- writing files is printed
- minor bugs fixed
- Seems like 0.92 rework was bullshit. damned. another rework

EVOLVE 0.92 rework ALMOST DONE
- fixed some bugs (have to load origin first)
- loading custom evolve_rates
- much faster and better results
- full integrity for full/grow method fo

plagih/modules/file_interaction.py
@@ -43,7 +43,6 @@ file_evolve_functions = 'run_files/evolve_functions.yaml'
 samples_csv = 'run_files/samples.csv'
 operators_csv = 'run_files/operators_csv.csv'
 operators_yaml = 'run_files/operators_csv.yaml'
-operators_info = 'run_files/operators_csv.yaml'
 distributions_file = 'run_files/distributions_file.yaml'
 tree_expr_txt = 'run_files/tree_expr.txt'
 tree_labels_csv = 'run_files/tree_labels.csv'
@@ -260,12 +259,7 @@ def plot_end(data_2d, plotname_path,
         print_e('Plotting empty array is not possible! Data={}'.format(data_2d))
         return
 
-    x, y = [], []
-    for a, b in data_2d:
-        x.append(a)
-        y.append(b)
-
-    # x, y = data_2d.reshape(-1, 2).T  # sfeh this could be a more pythonic way, but tuples can not be reshaped.
+    x, y = data_2d
 
     # bottom, top = plt.ylim()
     # left, right = plt.xlim()
@@ -277,19 +271,6 @@ def plot_end(data_2d, plotname_path,
         y = [new_top + 1] + y + [y[-1]]
 
     fig, ax = plt.subplots()
-
-    x = np.array(x)  # sfeh this could have been done earlier...
-    y = np.array(y)
-
-    if step_where:
-        ax.step(x, y, plt_xparam, linestyle=linestyle, marker=marker, label=plt_curve_label, where=step_where)
-    else:
-        ax.plot(x, y, plt_xparam, linestyle=linestyle, marker=marker, label=plt_curve_label)
-        if fill_variance:
-            pass  # todo
-            # fill_variance = np.array([x[1] for x in fill_variance.items()])  # first, extract 2nd val from tuple, then make the list a np-array
-            # ax.fill_between(x, y-fill_variance, y+fill_variance, alpha=0.2)
-
     ax.set_yscale(yscale)
     ax.set_ylim(min(bottom, 0), new_top)
     ax.set_xlim(min(left, 0), new_right)
@@ -298,6 +279,18 @@ def plot_end(data_2d, plotname_path,
     ax.set_xlabel(plt_x_label)
     ax.set_ylabel(plt_y_label)
     ax.set_title(plt_title)
+
+    if step_where:
+        ax.step(x, y, plt_xparam, linestyle=linestyle, marker=marker, label=plt_curve_label, where=step_where)
+    else:
+        ax.plot(x, y, plt_xparam, linestyle=linestyle, marker=marker, label=plt_curve_label)
+        if fill_variance is not None:
+            x_std, y_var = fill_variance
+            y_std = np.sqrt(y_var)
+            lower_bound_stderr = y-y_std
+            upper_bound_stderr = y+y_std
+            ax.fill_between(x_std, lower_bound_stderr, upper_bound_stderr, alpha=0.2)
+            ax.set_ylim(min(bottom, 0), new_top + np.max(upper_bound_stderr))
 
     # plt.legend()
     if subfolder:  #
@@ -311,8 +304,8 @@ def plot_end(data_2d, plotname_path,
 
     plt.tight_layout()
     plt.savefig(plotname_path / '{}.png'.format(plt_title))
-    # plt.close()  # Stackoverflow said that this is too much, clf should be better
-    plt.clf()
+    plt.close()  # Stackoverflow said that this is too much, clf should be better
+    # plt.clf()
     return
 
 #

plagih/modules/operators.py
@@ -54,48 +54,52 @@ op: Dict to work as 'Database' for every expression-bit and its features
 Features should always be defined, even though they might not occur at all. If not used, they CAN be filled with dummy values like äöü
 Some, which are known of not being used yet are commented with '# not tested' or '# not used'
 sfeh: write test that checks all operators_csv for sympificytion (...+branch-combinations, and more?)
+sfeh: use function-types (-> 'kommuttative'?)
 """
 op_what = {  # 'f2f': Classical mathematical operators_csv, evaluate from float to float
-    '+': {'fun': '+', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.add, 'latex1': '$+$', 'latexF': '{}+{}',
+    '+': {'fun': '+', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.add, 'opgroup': ['aritygroup'], 'latex1': '$+$', 'latexF': '{}+{}',
           'sym_str': '({} + {})', 'pycode': lambda a, b: '({}+{})'.format(a, b)},
-    '-': {'fun': '-', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.subtract, 'latex1': '$-$', 'latexF': '{}-{}',
+    '-': {'fun': '-', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.subtract, 'opgroup': ['aritygroup'], 'latex1': '$-$', 'latexF': '{}-{}',
           'sym_str': '({} - {})', 'pycode': lambda a, b: '({}-{})'.format(a, b)},
-    'usub': {'fun': 'usub', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.negative, 'latex1': '$-$', 'latexF': '-{}',
+    'usub': {'fun': 'usub', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.negative, 'opgroup': [], 'latex1': '$-$', 'latexF': '-{}',
              'sym_str': '(-{})', 'pycode': lambda a: '(-{})'.format(a)},
-    '*': {'fun': '*', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.multiply, 'latex1': '$\\cdot$', 'latexF': '{}\\cdot{}',
+    '*': {'fun': '*', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.multiply,  'opgroup': ['aritygroup'], 'latex1': '$\\cdot$', 'latexF': '{}\\cdot{}',
           'sym_str': '({} * {})', 'pycode': lambda a, b: '({}*{})'.format(a, b)},
     # Division: SAFE division by zero! -->tf.math.divide_no_nan -->pycode a/b --> div(a,b) !!pycode requires div_safe() implemented sfeh: is it okay to display this as '/'?
-    '/': {'fun': '/', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.math.divide_no_nan, 'latex1': '$\\div$', 'latexF': '\\frac{{{}}}{{{}}}',
+    '/': {'fun': '/', 'arity': 2, 'xtype': 'f2f', 'c-weight': 1, 'tf': tf.math.divide_no_nan, 'opgroup': [], 'latex1': '$\\div$', 'latexF': '\\frac{{{}}}{{{}}}',
           'sym_str': '({} / {})',
           'pycode': lambda a, b: '(lambda x, y: x/y if y!=0 else 0)({}, {})'.format(a, b)},
-    '**': {'fun': '**', 'arity': 2, 'xtype': 'f2f', 'c-weight': 2, 'tf': tf.pow, 'latex1': '$**$', 'latexF': '{{{}}}^{{{}}}',  # sfeh latexf requires some testing...
+    '**': {'fun': '**', 'arity': 2, 'xtype': 'f2f', 'c-weight': 2, 'tf': tf.pow,  'opgroup': [], 'latex1': '$**$', 'latexF': '{{{}}}^{{{}}}',  # sfeh latexf requires some testing...
            'sym_str': '({} ** {})', 'pycode': lambda a, b: '({}*{})'.format(a, b)},
 
-    'abs': {'fun': 'abs', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.math.abs, 'latex1': '$abs$', 'latexF': '|{{{}}}|',
+    'abs': {'fun': 'abs', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.math.abs, 'opgroup': [], 'latex1': '$abs$', 'latexF': '|{{{}}}|',
             'sym_str': 'abs({})', 'pycode': lambda a: 'abs({})'.format(a)},
-    'sign': {'fun': 'sign', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.math.sign, 'latex1': '$sign$', 'latexF': 'sign({{{}}})',
+    'sign': {'fun': 'sign', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.math.sign, 'opgroup': [], 'latex1': '$sign$', 'latexF': 'sign({{{}}})',
              'sym_str': 'sign({})', 'pycode': lambda a: 'np.sign({})'.format(a)},
-    'Square': {'fun': 'Square', 'arity': 1, 'xtype': 'f2f', 'c-weight': 2, 'tf': tf.math.square, 'latex1': '$x^2$', 'latexF': '{{{}}}^2',
+    
+    'Square': {'fun': 'Square', 'arity': 1, 'xtype': 'f2f', 'c-weight': 2, 'tf': tf.math.square, 'opgroup': [], 'latex1': '$x^2$', 'latexF': '{{{}}}^2',
                'sym_str': 'Square({})', 'pycode': lambda a: '({}**2)'.format(a)},
-    'sqrt': {'fun': 'sqrt', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.sqrt, 'latex1': '$\\sqrt{x}$', 'latexF': '\\sqrt{{{}}}',
+    'sqrt': {'fun': 'sqrt', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.sqrt, 'opgroup': [], 'latex1': '$\\sqrt{x}$', 'latexF': '\\sqrt{{{}}}',
              'sym_str': 'sqrt({})', 'pycode': lambda a: 'math.sqrt({})'.format(a)},
-    'log': {'fun': 'log', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.math.log, 'latex1': '$\\ln$', 'latexF': '\\ln{{{}}}',
+
+    'log': {'fun': 'log', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.math.log, 'opgroup': ['logs'], 'latex1': '$\\ln$', 'latexF': '\\ln{{{}}}',
             'sym_str': 'log({})', 'pycode': lambda a: 'math.log({})'.format(a)},  # python log is actual ln
-    'log1p': {'fun': 'log1p', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.math.log1p, 'latex1': '$\\log(1+x)$', 'latexF': '\\log(1+{{{}}})',
+    'log1p': {'fun': 'log1p', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.math.log1p, 'opgroup': ['logs'], 'latex1': '$\\log(1+x)$', 'latexF': '\\log(1+{{{}}})',
               'sym_str': 'log1p({})', 'pycode': lambda a: 'math.log1p({})'.format(a)},
-    'cos': {'fun': 'cos', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.cos, 'latex1': '$\\cos$', 'latexF': '\\cos({{{}}})',
+    
+    'cos': {'fun': 'cos', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.cos, 'opgroup': ['angle'], 'latex1': '$\\cos$', 'latexF': '\\cos({{{}}})',
             'sym_str': 'cos({})', 'pycode': lambda a: 'math.cos({})'.format(a)},
-    'sin': {'fun': 'sin', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.sin, 'latex1': '$\\sin$', 'latexF': '\\sin({{{}}})',
+    'sin': {'fun': 'sin', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.sin, 'opgroup': ['angle'], 'latex1': '$\\sin$', 'latexF': '\\sin({{{}}})',
             'sym_str': 'sin({})', 'pycode': lambda a: 'math.sin({})'.format(a)},
-    'tan': {'fun': 'tan', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.atan, 'latex1': '$\\tan$', 'latexF': '\\tan({{{}}})',
+    'tan': {'fun': 'tan', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.atan, 'opgroup': ['angle'], 'latex1': '$\\tan$', 'latexF': '\\tan({{{}}})',
             'sym_str': 'tan({})', 'pycode': lambda a: 'math.tan({})'.format(a)},
-    'acos': {'fun': 'acos', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.acos, 'latex1': '$\\acos$', 'latexF': '\\acos({{{}}})',
+    'acos': {'fun': 'acos', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.acos, 'opgroup': ['angle'], 'latex1': '$\\acos$', 'latexF': '\\acos({{{}}})',
              'sym_str': 'acos({})', 'pycode': lambda a: 'math.acos({})'.format(a)},
-    'asin': {'fun': 'asin', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.asin, 'latex1': '$\\asin$', 'latexF': '\\asin({{{}}})',
+    'asin': {'fun': 'asin', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.asin, 'opgroup': ['angle'], 'latex1': '$\\asin$', 'latexF': '\\asin({{{}}})',
              'sym_str': 'asin({})', 'pycode': lambda a: 'math.asin({})'.format(a)},
-    'atan': {'fun': 'atan', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.atan, 'latex1': '$\atan$', 'latexF': '\\atan({{{}}})',
+    'atan': {'fun': 'atan', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.atan, 'opgroup': ['angle'], 'latex1': '$\atan$', 'latexF': '\\atan({{{}}})',
              'sym_str': 'atan({})', 'pycode': lambda a: 'math.atan({})'.format(a)},
-    'tanh': {'fun': 'tanh', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.tanh, 'latex1': '$\tanh$', 'latexF': '\\tanh({{{}}})',
+    'tanh': {'fun': 'tanh', 'arity': 1, 'xtype': 'f2f', 'c-weight': 3, 'tf': tf.tanh, 'opgroup': ['angle'], 'latex1': '$\tanh$', 'latexF': '\\tanh({{{}}})',
              'sym_str': 'tanh({})', 'pycode': lambda a: 'math.tanh({})'.format(a)},
     # 'Integer': {'fun': 'Integer', 'arity': 1, 'xtype': 'f2f', 'c-weight': 0.5, 'tf': tf.cast({}, tf.int32), 'latex1': None, 'latexF': '{}', 
     # 'sym_str': 'N({}, )', 'pycode': lambda a: 'math.tanh({})'.format(a)},

plagih/modules/plagih_gp_base_class_xai.py
@@ -20,6 +20,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 
 sympy_dummy = plagih_sympify(1)
 np.set_printoptions(linewidth=320)  # set the terminal to  320 characters before line-wrapping in order to view Trees
+PLAGIH_VERSION = 0.95
 
 
 class ExplainableGP(object):
@@ -38,6 +39,7 @@ class ExplainableGP(object):
         self.root_dir = root_dir
 
         self.config = {
+            'pl_version': PLAGIH_VERSION,  # version important when loading old run
             'mode': 'run',  # ['run', 'analyse']
             'description': 'No description set',
             'choose': {
@@ -106,7 +108,7 @@ class ExplainableGP(object):
         self.kernel = FitnessKernel(self.config['kernel_name'])
         self.print_type = self.config['print_type']
         self.precision = self.config['precision']  # the number of floating points for the round function
-        self.parsimony_tmp = self.config['parsimony_tmp']
+        # self.parsimony_tmp = self.config['parsimony_tmp']
         self.parsimony_max = self.config['parsimony_max']
         self.monitor_dict = self.config['monitor']
         self.tourn_size = self.config['tourn_size']
@@ -144,8 +146,9 @@ class ExplainableGP(object):
                                 'total_found_trees': {},
                                 'gen_time': {},
                                 'complexity_average': {},
-                                'complexity_variance': {},
-                                'tmp_pop_fitness_distribution': {}}
+                                'complexity_variance': {},  # variance can be deleted, only std-error is needed
+                                'pop:trees:complexity:std_error': {}}
+                                # 'tmp_pop_fitness_distribution': {}# sfeh delete this version1
         self.pop_analysis_dict = {}  # similar to monitoring_dict
 
         self.print_g('ggg', 'Init. Time: {:4.2f}s'.format(time.perf_counter() - self.time_start))
@@ -189,6 +192,7 @@ class ExplainableGP(object):
                 self.env_variables['obs_name'][col_label]['core_label'] = core_label
             self.printpl('w', 'Attention, we updated self.env_variables for an old run')
             # todo should we save the update?
+
         return
 
     def load_backup_pickle(self, path_backup):
@@ -199,10 +203,22 @@ class ExplainableGP(object):
         with Path.open(path_backup, 'rb') as file:
             run_data = pickle.load(file)
 
-        self.restart_count, self.gen_id, self.parsimony_best_meta, self.pareto, self.population_base, self.monitoring_dict = run_data
+        try:
+            self.config['pl_version'], self.restart_count, self.gen_id, self.parsimony_best_meta, self.pareto, self.population_base, self.monitoring_dict = run_data
+            # todo test
+        except:
+            # todo remove this in version1
+            self.restart_count, self.gen_id, self.parsimony_best_meta, self.pareto, self.population_base, self.monitoring_dict = run_data
+            self.version = 0.9
+
         self.restart_count += 1
 
         self.update_old_runs()
+
+        # update monitoring dict (average vs variance)
+        if self.config['pl_version'] <= 0.9:
+            pass
+            # self.monitoring_dict['pop:trees:complexity:std_error'] = np.sqrt(self.monitoring_dict['complexity_variance'])
 
         printez('g', 'Loading Generation: {}'.format(self.gen_id), self.print_type)
 
@@ -216,7 +232,7 @@ class ExplainableGP(object):
         - Save valuable meta-data_csv_path: current generation (custom_done)
         """
 
-        run_backup_data = self.restart_count, self.gen_id, self.parsimony_best_meta, self.pareto, self.population_base, self.monitoring_dict
+        run_backup_data = self.config['pl_version'], self.restart_count, self.gen_id, self.parsimony_best_meta, self.pareto, self.population_base, self.monitoring_dict
 
         path_backup = file_make_dir(self.root_dir / file_backup_pickle)
         with Path.open(path_backup, 'wb') as file:
@@ -236,7 +252,7 @@ class ExplainableGP(object):
         self.write_config_yaml()  # sfeh or json?
 
         if self.gen_id == 0:
-            self.gen_create_first()
+            self.gen_create_initial()
 
         while self.run_continues():  # max generation, max time, done...
             self.printpl('gg', 'Evolving Generation {}'.format(self.gen_id))
@@ -288,7 +304,7 @@ class ExplainableGP(object):
                 return True
         return False
 
-    def gen_create_first(self):
+    def gen_create_initial(self):
         """
         Everything that needs to be custom_done for the first generation
         - Extracts "origin_meta Tree" from file
@@ -297,6 +313,7 @@ class ExplainableGP(object):
         - Monitoring initialisation and monitoring
         """
         self.print_g('gg', 'Preparing to create first Generation. Gen {}.'.format(self.gen_id))
+        self.gen_id = 0
         self.gen_reset_parameters()
 
         random_rate = 0  # The total amount of random creations in the evolve configuration. usually like 0.3 or so
@@ -307,7 +324,7 @@ class ExplainableGP(object):
         for ii, evolve_specs in enumerate(self.evolve_list):
             if evolve_specs['evolve_name'] == 'random trees':
                 # time_evolve = time.perf_counter()
-                evolve_num = int(self.config['gen_max'] * (evolve_specs['evolve_rate'] / random_rate))
+                evolve_num = int(self.config['pop_max'] * (evolve_specs['evolve_rate'] / random_rate))
                 call_params = evolve_specs.get('custom_params')
                 tag = evolve_specs['tag']
 
@@ -333,6 +350,7 @@ class ExplainableGP(object):
         # All gp creators: name, function, num of trees from tournament selection
         # sfehsfeh idee: print every written file
 
+        self.gen_id += 1
         self.gen_reset_parameters()
 
         # ########
@@ -446,10 +464,6 @@ class ExplainableGP(object):
         Every few generations, update the created gp_files
         - default is in every generation, but saving every n-th gen or after time passed is possible aswell
         """
-        if self.config['overwrite periodic gp_files']:
-            tmp_path = folder_make_dir(self.root_dir)
-        else:
-            tmp_path = folder_make_dir(self.root_dir / folder_steps / 'Gen-{}'.format(self.gen_id))
 
         time_now = time.perf_counter()
 
@@ -476,7 +490,11 @@ class ExplainableGP(object):
                 save_run = True
 
         if plots_show:
-            self.file_all_plots(tmp_path)
+            if self.config['overwrite periodic gp_files']:
+                subfolder = ''
+            else:
+                subfolder = Path(folder_steps / 'Gen-{}'.format(self.gen_id))
+            self.file_all_plots(self.root_dir, subfolder=subfolder)
 
         if save_run:
             self.run_backup_save()
@@ -1107,12 +1125,11 @@ class ExplainableGP(object):
         - Linearly increase threshold for parsimony
         """
 
-        self.gen_id += 1
         self.time_genstart = time.perf_counter()
         self.population_tmp_done = []
         self.population_tmp_eval = []
-        self.parsimony_tmp = max(1 / min(self.gen_id, self.config['gen_num_max_parsimony']) * self.parsimony_max,
-                                 self.parsimony_max)
+        # self.parsimony_tmp = max(1 / min(self.gen_id, self.config['gen_num_max_parsimony']) * self.parsimony_max,
+        #                          self.parsimony_max)
 
         return
 
@@ -1600,16 +1617,7 @@ class ExplainableGP(object):
     #   Monitoring                                +
     # +++++++++++++++++++++++++++++++++++++++++++++
 
-    def get_pareto_plot_values(self):
-        """
-        sfeh i think there is a more beautiful solution?
-        """
-        tuples = []
-        for key in sorted(self.pareto):
-            tuples.append((key, self.pareto[key]['fitness_train']))
-        return tuples
-
-    def file_all_plots(self, path):
+    def file_all_plots(self, root_path, subfolder=''):
         """
         Make all plots
         'fitness_average'
@@ -1620,27 +1628,39 @@ class ExplainableGP(object):
         'tmp_pop_fitness_distribution'
         """
 
-        path_plots = folder_make_dir(path / folder_plots)
+        path_plots = folder_make_dir(root_path / folder_plots / subfolder)
+
+        def plotendify_me(data_dict):
+            # sfeh delete this version1 (working only with np-arrays, no dicts)
+            npdata_dict = np.array([list(x) for x in sorted(data_dict.items())]).T
+            return npdata_dict
+
+        def get_pareto_plot_values():
+            # sfeh i think there is a more beautiful solution?
+            tuples = []
+            for key in sorted(self.pareto):
+                tuples.append([key, self.pareto[key]['fitness_train']])
+            npdata_dict = np.array(tuples).T
+            return npdata_dict
 
         if self.monitor_dict['gen_fitness_average'] == 'y':
-            data_tuples = sorted(list(self.monitoring_dict['fitness_average'].items()))
+            data_tuples = plotendify_me(self.monitoring_dict['fitness_average'])
             plot_end(data_tuples, path_plots, plt_title='average error', plt_y_label='fitness',
-                     set_left=data_tuples[0][0],
-                     fill_variance=self.monitoring_dict.get('fitness_variance'))
+                     set_left=data_tuples[0][0])
 
         if self.monitor_dict['population_tmp_done-size'] == 'y':
-            data_tuples = sorted(list(self.monitoring_dict['population_tmp_done-size'].items()))
+            data_tuples = plotendify_me(self.monitoring_dict['population_tmp_done-size'])
             plot_end(data_tuples, path_plots, plt_title='genepool size', plt_y_label='amount', linestyle='None',
                      marker='.',
                      set_left=data_tuples[0][0])
 
-        data_tuples = sorted(list(self.monitoring_dict['gen_time'].items()))
+        data_tuples = plotendify_me(self.monitoring_dict['gen_time'])
         plot_end(data_tuples, path_plots, plt_title='Generation time', plt_y_label='time', linestyle='None',
                  marker='.',
                  set_left=data_tuples[0][0])
 
-        data_tuples = self.get_pareto_plot_values()
-        plot_end(data_tuples, path, plt_title='pareto dominant candidates', plt_x_label='parsimony',
+        data_tuples = get_pareto_plot_values()
+        plot_end(data_tuples, root_path, plt_title='pareto dominant candidates', plt_x_label='parsimony',
                  plt_y_label='fitness',
                  linestyle='dashed',
                  marker='.',
@@ -1649,38 +1669,43 @@ class ExplainableGP(object):
                  beyond_lines=True,
                  save_tikz=True)
 
-        dist_fit = self.monitoring_dict['tmp_pop_fitness_distribution']
-        plot_end(dist_fit, path_plots, plt_title='population distribution Gen {}'.format(self.gen_id),
-                 plt_x_label='tree ids',
-                 plt_y_label='fitness',
-                 marker='',
-                 set_right=self.config['pop_max'],
-                 right_padding=1,
-                 subfolder=folder_pop_analysis)
-
         if self.monitor_dict.get('fitness_variance') == 'y':
-            data_tuples = sorted(list(self.monitoring_dict['fitness_variance'].items()))
+            data_tuples = plotendify_me(self.monitoring_dict['fitness_variance'])
             plot_end(data_tuples, path_plots, plt_title='variance in error', plt_y_label='variance',
                      marker='')
 
-        data_tuples = sorted(list(self.monitoring_dict['complexity_average'].items()))
-        plot_end(data_tuples, path_plots, plt_title='average tree complexity', plt_y_label='#nodes',
-                 set_left=data_tuples[0][0])
+        data_tuples = plotendify_me(self.monitoring_dict['complexity_average'])
+        data_tuples_variance = plotendify_me(self.monitoring_dict['complexity_variance'])  # sfeh update to standard error
+        plot_end(data_tuples, path_plots, plt_title='tree complexity (avg and std. error)', plt_y_label='variance',
+                 marker='', fill_variance=data_tuples_variance)
 
-        data_tuples = sorted(list(self.monitoring_dict['complexity_variance'].items()))
-        plot_end(data_tuples, path_plots, plt_title='variance in complexity', plt_y_label='variance',
-                 marker='')
-
-        data_tuples = sorted(list(self.monitoring_dict['total_found_trees'].items()))
+        data_tuples = plotendify_me(self.monitoring_dict['total_found_trees'])
         plot_end(data_tuples, path_plots, plt_title='number of created trees', plt_y_label='amount', linestyle='None',
                  marker='.',
                  set_left=data_tuples[0][0])
 
-        data_tuples = sorted(list(self.monitoring_dict['best_candidate'].items()))
+        data_tuples = plotendify_me(self.monitoring_dict['best_candidate'])
         plot_end(data_tuples, path_plots, plt_title='best candidate', plt_x_label='generation',
                  plt_y_label='error',
                  linestyle='dashed',
                  step_where='post')
+
+        # data_tuples = plotendify_me(self.monitoring_dict['complexity_average'])
+        # plot_end(data_tuples, path_plots, plt_title='average tree complexity', plt_y_label='#nodes',
+        #          set_left=data_tuples[0][0])
+        #
+        # data_tuples = plotendify_me(self.monitoring_dict['complexity_variance'])
+        # plot_end(data_tuples, path_plots, plt_title='variance in complexity', plt_y_label='variance',
+        #          marker='')
+
+        # data_tuples = np.array([list(x) for x in self.monitoring_dict['tmp_pop_fitness_distribution']]).T  # sfeh delete list version1 [(1,234),(2,544), ..]
+        # plot_end(data_tuples, path_plots, plt_title='population distribution Gen {}'.format(self.gen_id),
+        #          plt_x_label='tree ids',
+        #          plt_y_label='fitness',
+        #          marker='',
+        #          set_right=self.config['pop_max'],
+        #          right_padding=1,
+        #          subfolder=folder_pop_analysis)
 
         # sfeh https://github.com/linkedin/naarad/issues/114 UserWarning: Attempting to set identical bottom==top results
 
@@ -1725,8 +1750,8 @@ class ExplainableGP(object):
         self.monitoring_dict['best_candidate'][self.gen_id] = self.best_fitness
 
         # Tree fitness distribution
-        dist_fit = [(i, x) for i, x in enumerate([x['fitness'] for x in pop_tree_analysis])]  # sorted() was here based on fitness
-        self.monitoring_dict['tmp_pop_fitness_distribution'] = dist_fit
+        # dist_fit = [[i, x] for i, x in enumerate([x['fitness'] for x in pop_tree_analysis])]
+        # self.monitoring_dict['tmp_pop_fitness_distribution'] = dist_fit
 
         # Tree complexity
         complexity_sum = 0
@@ -1740,9 +1765,11 @@ class ExplainableGP(object):
         fitness_variance = np.var(todo)
         self.monitoring_dict['fitness_variance'][self.gen_id] = fitness_variance
 
-        # complexity variance
-        parsimony_variance = np.var([x['complexity'] for x in pop_tree_analysis])
-        self.monitoring_dict['complexity_variance'][self.gen_id] = parsimony_variance
+        # complexity standard-error
+        if delete_this and self.monitoring_dict.get('pop:trees:complexity:std_error') is None:
+            self.monitoring_dict['pop:trees:complexity:std_error'] = {}  # sfeh delete this version1
+        self.monitoring_dict['pop:trees:complexity:std_error'][self.gen_id] = np.std([x['complexity'] for x in pop_tree_analysis])
+        self.monitoring_dict['complexity_variance'][self.gen_id] = np.var([x['complexity'] for x in pop_tree_analysis])  # sfeh version1 delete this shit
 
         return
 

plagih/plagih_gp.py
@@ -8,7 +8,6 @@ from plagih.modules.plagih_data import *
 from plagih.modules.operators import oparray_from_list
 import yaml
 
-
 # import warnings
 # warnings.filterwarnings('error')
 
@@ -43,7 +42,7 @@ def labellists_from_csv(csv_path):
                 if row[0] == 'label_list' or row[0] == 'node_label':
                     label_list = [x.replace(' ', '') for x in row[1:]]
                 elif row[0] == 'modify_list' or row[0] == 'node_modify':
-                    modify_list = [int(x.replace(' ')) for x in row[1:]]
+                    modify_list = [int(x.replace(' ', '')) for x in row[1:]]
                 elif row[0] == '':
                     pass
                 else:
@@ -221,12 +220,12 @@ def load_tree_builders(root_dir, data_prepared=None):
 
     """
     # Load operators_csv
-    path = root_dir / operators_yaml
+    path_opyaml = root_dir / operators_yaml
     # if Path.is_file(operators_csv):  # sfeh test yaml
     #     functions = np.loadtxt(operators_csv, delimiter=',', skiprows=1, dtype=str)  # load the user defined functions (operators_csv)
 
-    if Path.is_file(path):
-        operators = yaml_load(path)
+    if Path.is_file(path_opyaml):
+        operators = yaml_load(path_opyaml)
     else:
         # raise FileNotFoundError('File does not exist: {}.'.format(operators_csv))
         print_warning('ww', 'Opt-in not specified. Operators-file does not exist. Creating one with a default list of mathematical operators_csv.')
@@ -253,7 +252,6 @@ def load_tree_builders(root_dir, data_prepared=None):
         #                       ['Tanh', 1], ['usub', 1],
         #                       ['+', 2], ['Ifte', 2]])
         # np.savetxt(operators_csv, functions, delimiter=',', fmt='%s')
-        yaml_dump(operators_info, operators)
 
     choose_oparray = oparray_from_list(operators)
 
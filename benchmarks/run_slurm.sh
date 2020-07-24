52a91cf3ebf3151b694430ca0705b492385e5a67
fehrer <fehrer@cip.ifi.lmu.de>
2020-07-24 23:36:15 +0200

    no real change

---
+++ benchmarks/run_slurm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#-*- coding:utf-8 -*-
+echo Starting run in C:\Users\Rapid\PycharmProjects\plagih\run_examples\slurm_runs\$1
+python3 C:\Users\Rapid\PycharmProjects\plagih\start.py -config_lookup $1

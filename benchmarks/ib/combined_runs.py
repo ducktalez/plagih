1e4956c781ec65d4ba2ef137c7d91f18becd9a0d
Simon Fehrer <sfehrer@gmx.de>
2020-11-16 02:32:10 +0100

    solved None-return bug in IB

--- benchmarks/ib/combined_runs.py
+++ benchmarks/ib/combined_runs.py
@@ -43,7 +43,7 @@ def mp_evall(arow):
     except Exception as ex:
         print(f'WARNING: Something failed in the evaluation process: {ex}')
         print(f'WARNING: arow {arow}')
-        return None  [lut_hash, None, None, None, None]
+        return [lut_hash, -20000, -20000, -20000, -20000]  # sfeh
 
 
 def eval_and_lut(eval_list, parsim_MAX, parsim_1MAX, lut_file, mp_cpu_MAX):
@@ -335,11 +335,11 @@ def main():
 
 
 if __name__ == '__main__':
-    # todotodo todo
-    lulzrow = {'parsim_sum': 27.0, 'parsims': [11.0, 6.0, 10.0], 'experiment': None, 'experiment_safe': None, 'experiment_r50': None, 'experiment_safe_r50': None,
-               'codes': ["((self.get_h('g', 8)*max(math.tanh(SetPoint), 0.091879))-max(0.197961, math.sqrt(self.get_h('v', 7))))", "max(math.tanh(-SetPoint), (self.get_h('g', 4)-self.get_h('g', 9)))",
-                         "(math.tanh(math.tanh(self.get_h('h', 4)))-((-self.get_h('f', 4)+self.get_h('f', 9))+math.sin(self.get_h('h', 9))))"], 'regress_sum': 2.504287,
-               'regress_vals': [0.915394, 0.80149, 0.787403]}
-
-    lulz = mp_evall(lulzrow)
-    # main()  # todo
+    # #  todo
+    # lulzrow = {'parsim_sum': 27.0, 'parsims': [11.0, 6.0, 10.0], 'experiment': None, 'experiment_safe': None, 'experiment_r50': None, 'experiment_safe_r50': None,
+    #            'codes': ["((self.get_h('g', 8)*max(math.tanh(SetPoint), 0.091879))-max(0.197961, math.sqrt(self.get_h('v', 7))))", "max(math.tanh(-SetPoint), (self.get_h('g', 4)-self.get_h('g', 9)))",
+    #                      "(math.tanh(math.tanh(self.get_h('h', 4)))-((-self.get_h('f', 4)+self.get_h('f', 9))+math.sin(self.get_h('h', 9))))"], 'regress_sum': 2.504287,
+    #            'regress_vals': [0.915394, 0.80149, 0.787403]}
+    #
+    # lulz = mp_evall(lulzrow)
+    main()

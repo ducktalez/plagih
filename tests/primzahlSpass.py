76479057dbabbe37447fdea9f51114c80738259c
fehrer <fehrer@cip.ifi.lmu.de>
2021-03-07 05:57:35 +0100

    v31 just before related work is rearranged

--- tests/primzahlSpass.py
+++ tests/primzahlSpass.py
@@ -6,6 +6,21 @@ import sys
 np.set_printoptions(precision=2, threshold=sys.maxsize, suppress=True)
 
 
+def help_running_sum(alist):
+    """
+    With leading zero? nope
+    """
+    return [b-a for a, b in zip(alist[1:], alist[:-1])]
+
+
+def binearspass():
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127]
+    primes2 = [[0, 1], [2, 3], [5, 7], [11, 13], [17, 19, 23, 29, 31], [37, 41, 43, 47, 53, 59, 61], [67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127]]
+    bins = [[bin(x) for x in xs] for xs in primes2]
+    for x in bins:
+        print(x)
+
+
 def altering_sum_plus_minus(pmax=1000):
     """
     Altering sum of primes; +2, -3, +5, -7
@@ -26,6 +41,7 @@ def altering_sum_plus_minus(pmax=1000):
 
     p_altsum_abs = [abs(x) for x in p_altsum]
     p_altsum_mean = [sum(p_altsum[0:x]) / len(p_altsum[0:x]) for x in range(1, len(p_altsum))]
+
     altsum_even_mean = np.array([(sum(p_even[0:x][::2]) - sum(p_even[1:x][::2])) / len(p_even[0:x]) for x in range(1, len(p_even))])
     altsum_odd_mean = np.array([(sum(p_odd[0:x][::2]) - sum(p_odd[1:x][::2])) / len(p_odd[0:x]) for x in range(1, len(p_odd))])
 
@@ -33,25 +49,42 @@ def altering_sum_plus_minus(pmax=1000):
     lul_remerge = np.array([item for t in lul_remerge for item in t])
     # print(lul_remerge)
     print(ppp)
-    print(p_altsum_abs)
+    print(p_altsum)
 
     # print(np.array(p_altsum_mean))
     t = p_altsum_mean
     hmmmm = np.array([abs(abs(j) - abs(i)) for i, j in zip(t[:-1], t[1:])])
     p_diff = [abs(p_altsum_abs[x] - p_altsum_abs[x - 1]) for x in range(1, len(p_altsum_abs))]
 
+    """another test"""
+    asdasd = help_running_sum(ppp)
+    print(asdasd)
+
     # print(p_altsum)
     # print(p_odd)
     # print(p_diff)
+
+
     with plt.rc_context(rc={}):
         fig, ax = plt.subplots()
-        ax.plot(p_altsum)
-        ax.plot([abs(x) for x in p_altsum])
-        plt.plot([p_diff[x] for x in p_diff])
-        ax.plot(p_diff)
-        # ax.plot(hmmmm)
-        # plt.plot(lul_remerge)
+        ax.plot(np.array(ppp[1:]), asdasd)
+        plt.show()
+
+        # ax.plot(p_altsum)
+        # ax.plot([abs(x) for x in p_altsum])
+        # ax.plot([p_diff[x] for x in p_diff])
+        # ax.plot(p_diff)
+        # # ax.plot(hmmmm)
+        # ax.plot(lul_remerge)
         # plt.plot(p_altsum[1::2])
+        ax.grid(True, linestyle='-.')
+
+
+def plottingspass(nmax=100):
+    approx_prime_count = [(x/(sympy.log(x))) for x in range(2, nmax)]
+    with plt.rc_context(rc={}):
+        fig, ax = plt.subplots()
+        ax.plot(approx_prime_count)
         ax.grid(True, linestyle='-.')
         plt.show()
 
